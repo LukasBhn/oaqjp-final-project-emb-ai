db2ce60a85bf8fcd8e99adeb491b274c8d5e1c3c
LukasBhn <1lukas.behnen@gmail.com>
2025-05-16 06:49:51 -0400

    Coursera DevOps - Final Project Flask Intro - Tasks 1-6

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the sentiment analysis API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    result = {}
+
+    emotions = ["anger", "disgust", "fear", "joy", "sadness"]
+    for emotion in emotions:
+        result[emotion] = formatted_response["emotionPredictions"][0]["emotion"][emotion]
+    
+    dominant_emotion = max(result, key=result.get)
+
+    result["dominant_emotion"] = dominant_emotion
+
+    return result
---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request 
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    score_anger = response["anger"]
+    score_disgust = response["disgust"]
+    score_fear = response["fear"]
+    score_joy = response["joy"]
+    score_sadness = response["sadness"]
+    dominant_emotion = response["dominant_emotion"]
+
+    return f"For the given statement, the system response is 'anger': {score_anger}, \
+    'disgust': {score_disgust}, 'fear': {score_fear}, 'joy': {score_joy} \
+    and 'sadness': {score_sadness}. The dominant emotion is {dominant_emotion}."
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
